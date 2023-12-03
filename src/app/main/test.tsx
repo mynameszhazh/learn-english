b75ea50461a42632aac3ea91ceef2d8ee05eb629
mynameszhazh <1594776259@qq.com>
2023-12-03 11:29:09 +0800

    ci: mvp 流程完成

--- src/app/main/test.tsx
+++ src/app/main/test.tsx
@@ -1,22 +1,71 @@
 import { ChangeEvent, useState } from "react";
 
+const failedCountTotal = 3;
+let failedCount = 0;
+
 export default function Test() {
+  const chinese = "现在";
+  const english = "now";
+  const answer = "now";
+  const soundmark = "/nav/";
+
   const [inputValue, setInputValue] = useState("");
+  const [currentValue, setCurrentValue] = useState(chinese);
+  const [dispatchSoundMark, setDispatchSoundMark] = useState(soundmark);
+  const [currentStatus, setCurrentStatus] = useState("question");
 
   function handleInputChange(e: ChangeEvent<HTMLInputElement>) {
     setInputValue(e.target.value);
   }
 
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === "Enter") {
-      setInputValue("");
+  function checkValidaty(value: string): boolean {
+    return value === answer;
+  }
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      if (checkValidaty(inputValue)) {
+        handleAnwser();
+      } else {
+        failedCount++;
+        setInputValue("");
+        setCurrentStatus("question");
+        console.log("提示重新输入");
+        if (failedCount > failedCountTotal) {
+          handleAnwser();
+        }
+      }
     }
   };
+
+  function handleAnwser() {
+    console.log("检验正确");
+    setCurrentValue(english);
+    setDispatchSoundMark(soundmark);
+    setCurrentStatus("anwser");
+    // 显示英文
+    console.log("读一下");
+  }
+  const anwserView = (
+    <div>
+      <div>{currentValue}</div>
+      <div>{dispatchSoundMark}</div>
+      <div>
+        <audio controls>
+          <source
+            src="https://dict.youdao.com/dictvoice?audio=now&type=1"
+            type="audio/mpeg"
+          ></source>
+        </audio>
+      </div>
+    </div>
+  );
+  const questionView = <div>{currentValue}</div>;
 
   return (
     <div>
       <div>
-        <div>现在</div>
+        {currentStatus === "question" ? questionView : anwserView}
         <div>
           <input
             type="text"
